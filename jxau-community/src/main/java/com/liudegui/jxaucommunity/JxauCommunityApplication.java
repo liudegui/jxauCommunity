3d55660556f4dbd54074271ac42e3ca4ed0f20e0
liudegui <liudegui1997@163.com>
2020-02-02 00:11:58 +0800

    first commit

---
+++ jxau-community/src/main/java/com/liudegui/jxaucommunity/JxauCommunityApplication.java
@@ -0,0 +1,13 @@
+package com.liudegui.jxaucommunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JxauCommunityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JxauCommunityApplication.class, args);
+    }
+
+}
