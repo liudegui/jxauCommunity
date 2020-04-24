30dc33e2b99e1ad1612548a73dbb6c79b2d3330d
liudegui <liudegui1997@163.com>
2020-04-24 23:20:14 +0800

    联系我们和安全退出，安全退出功能为实现

---
+++ jxau-community/src/main/java/com/liudegui/controller/ExitController.java
@@ -0,0 +1,13 @@
+package com.liudegui.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ExitController {
+
+    @RequestMapping("/exit")
+    public String exit(){
+        return "login";
+    }
+}
--- jxau-community/src/main/java/com/liudegui/controller/UpdatePasswordController.java
+++ jxau-community/src/main/java/com/liudegui/controller/UpdatePasswordController.java
@@ -4,7 +4,6 @@ import com.liudegui.base.Constant;
 import com.liudegui.base.exceptions.ParamsException;
 import com.liudegui.model.MessageModel;
 import com.liudegui.service.UpadatePwdService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
